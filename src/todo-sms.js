d970c426350a1e18e0e630848071216b3e98bd40
Alex Covington <acovington96@hotmail.com>
2019-07-30 10:01:26 -0700

    Added support for multiple numbers

--- src/todo-sms.js
+++ src/todo-sms.js
@@ -6,44 +6,59 @@ const scheduler = require('node-schedule');
 const fs = require('fs');
 const config = require('../config');
 
+const allNumbers = config.numbers.map(num => num.number);
 const messenger = twilio(config.sid, config.token);
-const queue = new Set();
+let queue = {};
+config.numbers.forEach((num) => {
+  queue[num.number] = {
+    snooze: 0,
+    list: new Set(),
+  };
+});
+console.log(queue);
 let snooze = 0;
 
-if (fs.existsSync('./queue-backup.json')) {
-  const backup = fs.readFileSync('./queue-backup.json');
-  backup.forEach(item => queue.add(item));
-}
+// if (fs.existsSync('./queue-backup.json')) {
+//   const backup = fs.readFileSync('./queue-backup.json', 'utf8');
+//   queue = JSON.parse(backup);
+// }
 
 const library = {
-  'ls': () => {
-    let items = Array.from(queue);
+  'ls': (_, num) => {
+    const cur = queue[num];
+    let items = Array.from(cur.list);
     if (items.length === 0) return 'You have nothing left in the queue'
     items = items.map((item, idx) => `${idx+1}. ${item}`);
     return `You have the following items left to do:\n${items.join('\n')}`;
   },
-  'rm': () => {
-    queue.clear();
+  'rm': (_, num) => {
+    const cur = queue[num];
+    cur.list.clear();
     return 'Cleared the queue.';
   },
-  '+': (args) => {
+  '+': (args, num) => {
     if (!args) return 'Nothing to add.';
-    args.split(',').forEach(arg => queue.add(arg.trim()));
+    const cur = queue[num];
+    console.log(queue, queue[num])
+    console.log(cur);
+    args.split(',').forEach(arg => cur.list.add(arg.trim()));
     return `Added ${args} to the queue.`;
   },
-  '-': (args) => {
+  '-': (args, num) => {
     if (!args) return 'Nothing to remove';
-    items = Array.from(queue);
+    const cur = queue[num];
+    items = Array.from(cur.list);
     const removed = [];
     args.split(',').forEach((arg) => {
       removed.push(items[Number(arg)-1]);
-      queue.delete(items[Number(arg)-1]);  
+      cur.list.delete(items[Number(arg)-1]);  
     });
     return `Removed ${removed.join(', ')} from the queue.`;
   },
-  'snooze': () => {
-    snooze++;
-    return `Current snooze value: ${snooze}`;
+  'snooze': (_, num) => {
+    const cur = queue[num];
+    cur.snooze++;
+    return `Current snooze value: ${cur.snooze}`;
   }
 };
 
@@ -58,44 +73,49 @@ app.use((req, res, next) => {
 
 app.post(`/todo/receive`, (req, res) => {
   const reply = new MessagingResponse();
-  const { Body } = req.body;
+  const { Body, From } = req.body;
   const cmd = Body.split(' ');
-  if (Object.prototype.hasOwnProperty.call(library, cmd[0].toLowerCase())) {
-    reply.message(library[cmd[0].toLowerCase()](cmd.length ? cmd.slice(1).join(' ') : null));
-  } else {
-    reply.message(`Unknown command ${cmd[0].toLowerCase()}`);
-  }
-  res.send(reply.toString());
+    if (Object.prototype.hasOwnProperty.call(library, cmd[0].toLowerCase())) {
+      reply.message(library[cmd[0].toLowerCase()](cmd.length ? cmd.slice(1).join(' ') : null, From.substr(2)));
+    } else {
+      reply.message(`Unknown command ${cmd[0].toLowerCase()}`);
+    }
+    res.send(reply.toString()); 
 });
 
 app.listen(config.port, () => console.log(`Listening on ${config.port}`));
 
 // Automatic text 
 scheduler.scheduleJob('0 10-22/2 * * *', () => {
-  if (snooze > 0) {
-    snooze--;
-    return;
-  }
-  let items = Array.from(queue);
-  if (items.length) {
-    items = items.map((item, idx) => `${idx+1}. ${item}`);
-    const message = `You have the following items left:\n${items.join('\n')}`;
-    messenger.messages.create({
-      body: message,
-      to: `+1${config.number}`,
-      from: `+1${config.twilioNumber}`,
-    }).then(() => console.log('Sent scheduled SMS reminder')).done();
-  }
+  allNumbers.forEach((num) => {
+    const cur = queue[num];
+    if (cur.snooze > 0) {
+      cur.snooze--;
+      return;
+    }
+    let items = Array.from(cur.list);
+    if (items.length) {
+      items = items.map((item, idx) => `${idx+1}. ${item}`);
+      const message = `You have the following items left:\n${items.join('\n')}`;
+      messenger.messages.create({
+        body: message,
+        to: `+1${num}`,
+        from: `+1${config.twilioNumber}`,
+      }).then(() => console.log('Sent scheduled SMS reminder')).done();
+    }  
+  });
 });
 
 // Load recurrent tasks
-if (config.recurrent) {
-  Object.keys(config.recurrent).forEach((task) => {
-    scheduler.scheduleJob(config.recurrent[task], () => queue.add(task));
-  });
-}
+config.numbers.forEach((num) => {
+  if (num.recurrent) {
+    Object.keys(num.recurrent).forEach((task) => {
+      scheduler.scheduleJob(num.recurrent[task], () => queue[num.number].list.add(task));
+    });
+  }
+});
 
 // Save in-memory list to disk in case of restart
 setInterval(() => {
-  fs.writeFileSync('./queue-backup.json', JSON.stringify(Array.from(queue)));
+  fs.writeFileSync('./queue-backup.json', JSON.stringify(queue));
 }, 60000);
